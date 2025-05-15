c32fdd0eee9a090ba1c9e08ad8e486506f374d3b
neerajguleria1 <neerajguleria30@gmail.com>
2025-05-15 18:02:23 +0530

    Add files via upload

---
+++ src/components/Login.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+    const [formData, setFormData] = useState({ email: '', password: '' });
+    const navigate = useNavigate();
+
+    const handleChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post("http://localhost:5000/api/users/login", formData);
+            localStorage.setItem("user", JSON.stringify(res.data.user));
+            navigate('/profile');
+        } catch (error) {
+            alert("Invalid credentials!");
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="email" name="email" placeholder="Email" onChange={handleChange} />
+            <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+            <button type="submit">Login</button>
+        </form>
+    );
+}
+
+export default Login;
